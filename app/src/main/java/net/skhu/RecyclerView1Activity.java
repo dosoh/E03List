cc560c12ee57e579ca8a3902b8097827548469c6
Kim, Hyojin <dosoh77@gmail.com>
2020-05-06 00:43:55 +0900

    ListView RecyclerView 예제구현

---
+++ app/src/main/java/net/skhu/RecyclerView1Activity.java
@@ -0,0 +1,14 @@
+package net.skhu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecyclerView1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view1);
+    }
+}
